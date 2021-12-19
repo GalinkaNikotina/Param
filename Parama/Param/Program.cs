7bd04187ce80513d17b72703143f55d125672bdc
Galinka <galinka2002niko@gmail.com>
2021-12-20 00:07:03 +0300

    po

---
+++ Parama/Param/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Param
+{
+    class Program
+    {
+        public class AClass1<T>
+        {
+            private T[] imyArray = new T[20];
+        }
+        public class M
+        {
+            static void Main(string[] args)
+            {
+                AClass1<string> K = new AClass1<string>();
+                AClass1<int> K2 = new AClass1<int>();
+            }
+        }
+    }
+}
+  
